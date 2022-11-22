778fd3fc96c540cb8eb1b6312486b9ea52690258
Aniket Raj <84118141+aniketraj18@users.noreply.github.com>
2022-11-22 20:31:11 +0530

    Update App.js
    Dark mode Enabled

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-// import logo from './logo.svg';
 import './App.css';
-import Navbar from'./Component/Navbar.js';
+import Navbar from './Component/Navbar.js';
 import Textform from'./Component/Textform.js';
+import React, {useState} from 'react';
+// import About from './Component/About.js';
 
 function App() {
+ const[mode,setMode]=useState('light');//Whether dark mode is enabled or not
+ const toggleMode=()=>{
+  if(mode==='dark'){
+  setMode('light');
+  document.body.style.backgroundColor='white';}
+  else{
+  setMode('dark');
+  document.body.style.backgroundColor='#3b506e';}
+
+ }
   return (
-    <><Navbar title="Word Analyser"/>
-    <div className="container"><Textform heading="Enter Your Text "/></div>
+    <><Navbar title="Word Analyser" mode={mode} toggleMode={toggleMode}/>
+    <div className="container"><Textform heading="Enter Your Text"  mode={mode}/></div>
+    <div>
+    </div>
     
     </>
   );
 }
 
 export default App;
+
